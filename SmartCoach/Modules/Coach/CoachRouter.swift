49ccf3b1edc63e9d9a1db4398e9292edc3110294
Tong Lin <tong.lin@gpshopper.com>
2018-12-18 18:18:36 -0500

    Added Coach view controller

---
+++ SmartCoach/Modules/Coach/CoachRouter.swift
@@ -0,0 +1,13 @@
+//
+//  CoachRouter.swift
+//  SmartCoach
+//
+//  Created by Tong Lin on 12/18/18.
+//  Copyright © 2018 TongL. All rights reserved.
+//
+
+import Foundation
+
+class CoachRouter {
+
+}
---
+++ SmartCoach/Modules/Coach/CoachViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CoachViewController.swift
+//  SmartCoach
+//
+//  Created by Tong Lin on 12/18/18.
+//  Copyright © 2018 TongL. All rights reserved.
+//
+
+import UIKit
+
+class CoachViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
