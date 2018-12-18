920ef0be975d2aadff099926a298b8107f61a6b0
Tong Lin <tong.lin@gpshopper.com>
2018-12-18 17:55:18 -0500

    init xcode project

---
+++ SmartCoach/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartCoach
+//
+//  Created by Tong Lin on 12/18/18.
+//  Copyright © 2018 TongL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
